6dab3b1641bebb6b486ebd3ef73534cea141d3e3
miraetto <miraetto@gmail.com>
2026-06-19 19:41:14 +0900

    Use Paperlogy for display typography

--- MiraeMotion/app/layout.tsx
+++ MiraeMotion/app/layout.tsx
@@ -50,10 +50,6 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
-          href="https://fonts.googleapis.com/css2?family=Gugi&display=swap"
-          rel="stylesheet"
-        />
-        <link
           href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@100..900&display=swap"
           rel="stylesheet"
         />
